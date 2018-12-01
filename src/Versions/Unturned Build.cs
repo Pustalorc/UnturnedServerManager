0adc0be3b7ffae267b74ce119dad2267b7d448ee
persiafighter <persiafighter@gmail.com>
2018-12-01 12:39:27 +0000

    Disabled unturnedbuild fetching
    Due to the website where the build was retrieved from going down, I have decided to simply make the UnturnedBuild return N/A to prevent crashes.

--- src/Versions/Unturned Build.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace ATORTTeam.UnturnedServerManager.Versions
-{
-    public static class UnturnedBuild
-    {
-        public static string Value => new WebClient().DownloadString("http://update.unturned.hosting").Trim();
-    }
-}
---
+++ src/Versions/UnturnedBuild.cs
@@ -0,0 +1,7 @@
+﻿namespace ATORTTeam.UnturnedServerManager.Versions
+{
+    internal static class UnturnedBuild
+    {
+        public static string Value => "N/A"; //new WebClient().DownloadString("http://update.unturned.hosting").Trim();
+    }
+} // Change this alright thanks mate.
